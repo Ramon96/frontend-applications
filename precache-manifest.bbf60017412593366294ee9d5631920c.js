c0c9b59479a80e4dfe00901045f9eed3369aee41
Ramon96 <rfm1@live.nl>
2019-10-31 20:17:30 +0100

    Updates

---
+++ precache-manifest.bbf60017412593366294ee9d5631920c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0251a90bcf67cd8d306445a761a77ff0",
+    "url": "/frontend-applications/index.html"
+  },
+  {
+    "revision": "ebfb4f3f2d2293008825",
+    "url": "/frontend-applications/static/css/main.99c9fe06.chunk.css"
+  },
+  {
+    "revision": "7beaf31c18cc6930292e",
+    "url": "/frontend-applications/static/js/2.ddc3f729.chunk.js"
+  },
+  {
+    "revision": "ebfb4f3f2d2293008825",
+    "url": "/frontend-applications/static/js/main.741edea6.chunk.js"
+  },
+  {
+    "revision": "1c6a7d2e42cf7eabcc40",
+    "url": "/frontend-applications/static/js/runtime-main.5f55b556.js"
+  }
+]);
