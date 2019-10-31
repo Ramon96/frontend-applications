8c2b810ab5ded6a257be2d47a9e19e53cf4a62b3
Ramon96 <rfm1@live.nl>
2019-10-31 09:10:01 +0100

    removed the basic react files, retrieved data from my endpoint and got a basic folder structure going

---
+++ frontend-application/src/components/page/doodle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ObjectDescription from '../ObjectDescription';
+
+
+class doodle extends Component {
+    render() {
+        return (
+            <div>
+                <ObjectDescription />
+            </div>
+        )
+    }
+}
+
+export default doodle;
