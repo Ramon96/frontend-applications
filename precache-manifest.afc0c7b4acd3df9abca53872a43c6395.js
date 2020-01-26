b76bb4d98a531c5fde18c824860d9325606279ee
Ramon96 <rfm1@live.nl>
2020-01-26 15:22:41 +0100

    Updates

---
+++ precache-manifest.afc0c7b4acd3df9abca53872a43c6395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6e0ce70bf162b745503155c7b7b174",
+    "url": "/frontend-applications/index.html"
+  },
+  {
+    "revision": "4bad20e88ad915f99e61",
+    "url": "/frontend-applications/static/css/main.96626f55.chunk.css"
+  },
+  {
+    "revision": "7b1dc14e4dbf2ae75bbc",
+    "url": "/frontend-applications/static/js/2.7f107c3a.chunk.js"
+  },
+  {
+    "revision": "4bad20e88ad915f99e61",
+    "url": "/frontend-applications/static/js/main.fbd66b05.chunk.js"
+  },
+  {
+    "revision": "1c6a7d2e42cf7eabcc40",
+    "url": "/frontend-applications/static/js/runtime-main.5f55b556.js"
+  },
+  {
+    "revision": "533c34fefea2331606b9513c25f3e9bc",
+    "url": "/frontend-applications/static/media/startover.533c34fe.png"
+  },
+  {
+    "revision": "fee5f09ba433ffa631389c3fca3869a8",
+    "url": "/frontend-applications/static/media/toys.fee5f09b.png"
+  }
+]);
